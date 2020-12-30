1e074678ee0f6a37ba10fd16930e69d31a0237d5
Richard Kasperowski <r@kasperowski.com>
2020-12-30 15:23:26 -0500

    A little more doc

--- src/test/renameme.spec.ts
+++ src/test/renameme.spec.ts
@@ -4,8 +4,9 @@
 // // Import anything you need from your ../app/foo.ts files. Here's an example from the Gilded Rose Refactoring Kata.
 // import { Item, GildedRose } from '../app/gilded-rose';
 //
-// // Change your project name. Write some tests. Enjoy!
+// // Change your project name.
 // describe('My Project Name', function () {
+// // We are using the Mocha test framework. See https://mochajs.org for more info. Write some tests. Enjoy!
 //
 //     // Here's your first Mocha test: make sure Mocha works properly.
 //     it('', function() {
