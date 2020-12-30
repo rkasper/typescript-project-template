33d4fc6c6a937de845cdc92ef35df020ea9db33b
Richard Kasperowski <r@kasperowski.com>
2020-12-30 15:11:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TypeScript Project Template
+This repo will get you started on your next TypeScript project. To use it, I follow these steps:
+
+- In WebStorm, create a new project from a Git repository. Use this repo's URL as the repo to clone.
+- Open file 'src/package.json'. Notice that a prompt pops up in the bottom-right corner of WebStorm:
+
+  Install dependencies
+  From src/package.json
+  Run 'npm install'     Don't ask again
+  
+- Select "Run 'npm install'".
+- 
---
+++ src/test/renameme.spec.ts
@@ -0,0 +1,15 @@
+// // Import Chai assertions. See https://www.chaijs.com/guide/styles/ for more info.
+// import { assert } from 'chai';
+//
+// // Import anything you need from your ../app/foo.ts files. Here's an example from the Gilded Rose Refactoring Kata.
+// import { Item, GildedRose } from '../app/gilded-rose';
+//
+// // Change your project name. Write some tests. Enjoy!
+// describe('My Project Name', function () {
+//
+//     // Here's your first Mocha test: make sure Mocha works properly.
+//     it('', function() {
+//         assert.isTrue(false);
+//     });
+//
+// });
